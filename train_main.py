6be5d1c7f3389b9c274c90832c8d6f9db4e5e61e
Unknown <15388951707@163.com>
2018-05-06 10:32:24 +0800

    update

--- train_main.py
+++ train_main.py
@@ -23,7 +23,7 @@ print(val_pd.shape)
 data_transforms = {
     'train': transforms.Compose([
         transforms.RandomRotation(degrees=15),
-        transforms.RandomResizedCrop(224),
+        transforms.RandomResizedCrop(224,scale=(0.49,1.0)),
         # transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),   # 0-255 to 0-1
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
